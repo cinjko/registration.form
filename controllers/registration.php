3d8c9234b76e1ace6b9d4a73e35deac92ddf1cf5
cinjko <cinjko21@gmail.com>
2015-03-21 18:12:14 +0200

    ів

---
+++ controllers/registration.php
@@ -0,0 +1,40 @@
+<?
+//include("models/registration.php");
+
+class Registration
+{
+	function __construct()
+	{
+
+	}
+
+	function registration()
+	{
+		include("models/registration.php");
+		$userLanguage = $_GET['id']; // отримуємо ідентифікатор мови
+
+		if (empty($userLanguage)){
+			$userLanguage = Language::browserLang();
+		}
+
+		$lang = Language::getLang($userLanguage);
+		$getCountry = RegistrationModel::getCountry($userLanguage);
+		$selectCountry = RegistrationModel::buildSelect($getCountry, 'country', 0);
+
+		$getProgramLanguich = RegistrationModel::getProgramLanguich();
+		$selectProfession = RegistrationModel::buildSelect($getProgramLanguich, 'profession', 1);
+
+		include("views/registration/registration.php");
+
+		$action = $this->saveregistration();
+}
+
+	function saveregistration()
+	{
+			print_r("registration success");
+
+	}
+
+
+}
+?>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?
+include ('/models/language.php');
+include ('/controllers/registration.php');
+
+$registration = new Registration();
+$registration->registration();
+?>
