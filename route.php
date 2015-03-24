3b742e42a0cb91d0ffe30b61214987f117341e9b
cinjko <cinjko21@gmail.com>
2015-03-24 22:58:24 +0200

    Chenged .gitignire

--- route.php
+++ route.php
@@ -3,17 +3,22 @@ class Rout{
 
     static function start(){
 
+        $url = $_SERVER['REQUEST_URI'];
+        print_r($url);die;
+//        if(!$controller_name){
+//            $controller_name = "main";
+//        }
+
         $controller_name = "main";
-
-
         $controller_name ="controller_".$controller_name;
         $controller_file = $controller_name.'.php';
         $controller_path = "controller/".$controller_file;
+        print_r($controller_path);die;
 
         if(file_exists($controller_path)){
             include_once $controller_path;
         }else{
-            require_once "controllers/main.php";
+//            require_once "controllers/main.php";
         }
     }
 }
